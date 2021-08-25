3e6168f934412c2f27159ad2c90582beaf4b7efd
Meurant <gaetanmeurant@gmail.com>
2021-08-25 15:07:17 +0200

    NoteDetailActivity + Parcelable + Intent

--- app/src/main/java/com/example/notepad/NoteDetailActivity.kt
+++ app/src/main/java/com/example/notepad/NoteDetailActivity.kt
@@ -2,10 +2,32 @@ package com.example.notepad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class NoteDetailActivity : AppCompatActivity() {
+
+    companion object {
+        val EXTRA_NOTE = "note"
+        val EXTRA_NOTE_INDEX = "noteIndex"
+    }
+
+    lateinit var note: Note
+    var noteIndex: Int = -1
+
+    lateinit var titleView: TextView
+    lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_detail)
+
+        note = intent.getParcelableExtra<Note>(EXTRA_NOTE)
+        noteIndex = intent.getIntExtra(EXTRA_NOTE_INDEX, -1)
+
+        titleView = findViewById<TextView>(R.id.title)
+        textView = findViewById<TextView>(R.id.text)
+
+        titleView.text = note.title
+        textView.text = note.text
     }
 }
--- app/src/main/java/com/example/notepad/NoteListActivity.kt
+++ app/src/main/java/com/example/notepad/NoteListActivity.kt
@@ -1,5 +1,6 @@
 package com.example.notepad
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -30,7 +31,15 @@ class NoteListActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(view: View) {
         if (view.tag != null) {
-            Log.i("NoteListActivity", "NoteList Clicked !!")
+            showNoteDetail(view.tag as Int)
         }
+    }
+
+    private fun showNoteDetail(noteIndex: Int) {
+        val note = notes[noteIndex]
+        val intent = Intent(this, NoteDetailActivity::class.java)
+        intent.putExtra(NoteDetailActivity.EXTRA_NOTE, note)
+        intent.putExtra(NoteDetailActivity.EXTRA_NOTE_INDEX, noteIndex)
+        startActivity(intent)
     }
 }
